7d6b8bd99124337a3081d6eeed32ae88a79d1432
Lucas Camarino Emerick de Almeida <lucascamarino@gmail.com>
2024-09-12 03:57:34 -0300

    ferrou
    se a solução iterativa ta dando prolema no grafo de 50000 vertices imagina ao tentar realizar a versão recursiva

--- Grafos/BuscaEmGrafos/iterativeWay.cpp
+++ Grafos/BuscaEmGrafos/iterativeWay.cpp
@@ -123,136 +123,153 @@ pair<vector<int>, vector<int>> readGraph(const string &filename)
 void classifyEdges(const vector<int> &pointer, const vector<int> &arc_dest, int Vx)
 {
     auto start = chrono::high_resolution_clock::now();
-    stack<int> pilha;
-    // pointer.size() - 2 é o numero de vertices, pois não estamos utilizando a pos 0, então temos q alocar numero de vertices mais um
-    // TD = tempo de descoberta, TT = tempo de termino
-    int TD[pointer.size() - 1], TT[pointer.size() - 1];
-    int t = 0;
-    EdgeClassification edgeClassification[arc_dest.size()];
-    fill_n(edgeClassification, arc_dest.size(), NEW_EDGE);
-    fill_n(TD, pointer.size() - 1, 0);
-    fill_n(TT, pointer.size() - 1, 0);
-
-    int vertice_atual;
-    // procura se tem algum vertice não explorado dentro de todos os vertices do grafo
-    for (int i = 1; i < pointer.size() - 1; i++)
+    try
     {
-        if (TD[i] == 0)
+        stack<int> pilha;
+        // pointer.size() - 2 é o numero de vertices, pois não estamos utilizando a pos 0, então temos q alocar numero de vertices mais um
+        // TD = tempo de descoberta, TT = tempo de termino
+        int TD[50001], TT[50001];
+        int t = 0;
+        EdgeClassification edgeClassification[arc_dest.size()];
+        fill_n(edgeClassification, arc_dest.size(), NEW_EDGE);
+        for(int i = 1; i < 50001 - 1; i++)
         {
-            pilha.push(i);
-            t++;
-            TD[i] = t;
+            TD[i] = TT[i] = 0;
         }
-        while (!pilha.empty())
-        {
-            vertice_atual = pilha.top();
+        int vertice_atual;
 
-            // variavel para marcar caso ache alguma aresta de arvore para aprofundar mais a busca
-            bool achou = false;
-            // itera as arestas que saem do vertice atual
-            for (int j = pointer[vertice_atual]; j < pointer[vertice_atual + 1]; j++)
+        // procura se tem algum vertice não explorado dentro de todos os vertices do grafo
+        for (int i = 1; i < pointer.size() - 1; i++)
+        {
+            if (TD[i] == 0)
             {
-                // se essa aresta nunca foi inspecionada
-                if (edgeClassification[j] == NEW_EDGE)
+                pilha.push(i);
+                t++;
+                TD[i] = t;
+            }
+            while (!pilha.empty())
+            {
+                if (pilha.size() % 10 == 0)
                 {
-                    // arc_dest[j] representa o vertice destino da aresta j
-                    if (TD[arc_dest[j]] == 0)
+                    
+                    cout << pilha.size();
+                    system("CLS");
+                }
+
+                vertice_atual = pilha.top();
+
+                // variavel para marcar caso ache alguma aresta de arvore para aprofundar mais a busca
+                bool achou = false;
+                // itera as arestas que saem do vertice atual
+                for (int j = pointer[vertice_atual]; j < pointer[vertice_atual + 1]; j++)
+                {
+                    // se essa aresta nunca foi inspecionada
+                    if (edgeClassification[j] == NEW_EDGE)
                     {
-                        edgeClassification[j] = TREE_EDGE;
-                        achou = true;
-                        pilha.push(arc_dest[j]);
-                        t++;
-                        TD[arc_dest[j]] = t;
-                        break;
-                    }
-                    else
-                    {
-                        if (TT[arc_dest[j]] == 0)
+                        // arc_dest[j] representa o vertice destino da aresta j
+                        if (TD[arc_dest[j]] == 0)
                         {
-                            edgeClassification[j] = BACK_EDGE;
-                        }
-                        else if (TD[vertice_atual] < TD[arc_dest[j]])
-                        {
-                            edgeClassification[j] = FORWARD_EDGE;
+                            edgeClassification[j] = TREE_EDGE;
+                            achou = true;
+                            pilha.push(arc_dest[j]);
+                            t++;
+                            TD[arc_dest[j]] = t;
+                            break;
                         }
                         else
                         {
-                            edgeClassification[j] = CROSS_EDGE;
+                            if (TT[arc_dest[j]] == 0)
+                            {
+                                edgeClassification[j] = BACK_EDGE;
+                            }
+                            else if (TD[vertice_atual] < TD[arc_dest[j]])
+                            {
+                                edgeClassification[j] = FORWARD_EDGE;
+                            }
+                            else
+                            {
+                                edgeClassification[j] = CROSS_EDGE;
+                            }
                         }
                     }
                 }
-            }
-            if (!achou)
-            {
-                // não foi encontrada nenhuma aresta de arvore então chegou o tempo de termino do vertice do topo da pilha
-                t++;
-                TT[vertice_atual] = t;
-                pilha.pop();
-            }
-        }
-    }
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
-    auto seconds = duration.count() / 1000;
-    auto minutes = seconds / 60;
-    seconds %= 60;
-    auto milliseconds = duration.count() % 1000;
-
-    cout << "Classificacao das arestas feitas em: " << minutes << " minutos, " << seconds << " segundos e " << milliseconds << " milissegundos" << endl;
-
-    // irei colocar na mesma função a escrita no arquivo do resultado das classificações das arestas
-    ofstream outputFile("tree_edges.txt");
-    if (outputFile)
-    {
-        outputFile << "Arestas de arvore: " << endl;
-        for (int i = 1; i < pointer.size() - 1; i++)
-        {
-            for (int j = pointer[i]; j < pointer[i + 1]; j++)
-            {
-                if (edgeClassification[j] == TREE_EDGE)
+                if (!achou)
                 {
-                    outputFile << i << " -> " << arc_dest[j] << endl;
+                    // não foi encontrada nenhuma aresta de arvore então chegou o tempo de termino do vertice do topo da pilha
+                    t++;
+                    TT[vertice_atual] = t;
+                    pilha.pop();
                 }
             }
         }
-        outputFile.close();
-        cout << "Classificacao das arestas escrita no arquivo: tree_edges.txt" << endl;
-    }
-    else
-    {
-        cerr << "Error: nao foi possivel escrever no arquivo tree_edges.txt" << endl;
-    }
 
-    // irei colocar na mesma função a escrita no arquivo do resultado das classificações das arestas que saem do verticeX
-    ofstream outputFileX("vertex_edges_classification.txt");
-    if (outputFileX)
-    {
-        outputFileX << "Classificacao das arestas que saem do vertice " << Vx << endl;
-        for (int i = pointer[Vx]; i < pointer[Vx + 1]; i++)
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        auto seconds = duration.count() / 1000;
+        auto minutes = seconds / 60;
+        seconds %= 60;
+        auto milliseconds = duration.count() % 1000;
+
+        cout << "Classificacao das arestas feitas em: " << minutes << " minutos, " << seconds << " segundos e " << milliseconds << " milissegundos" << endl;
+
+        // irei colocar na mesma função a escrita no arquivo do resultado das classificações das arestas
+        ofstream outputFile("tree_edges.txt");
+        if (outputFile)
         {
-            outputFileX << Vx << " -> " << arc_dest[i];
-            switch (edgeClassification[i])
+            outputFile << "Arestas de arvore: " << endl;
+            for (int i = 1; i < pointer.size() - 1; i++)
             {
-            case TREE_EDGE:
-                outputFileX << " Aresta de árvore" << endl;
-                break;
-            case BACK_EDGE:
-                outputFileX << " Aresta de retorno" << endl;
-                break;
-            case FORWARD_EDGE:
-                outputFileX << " Aresta de avanço" << endl;
-                break;
-            case CROSS_EDGE:
-                outputFileX << " Aresta de cruzamento" << endl;
-                break;
+                for (int j = pointer[i]; j < pointer[i + 1]; j++)
+                {
+                    if (edgeClassification[j] == TREE_EDGE)
+                    {
+                        outputFile << i << " -> " << arc_dest[j] << endl;
+                    }
+                }
             }
+            outputFile.close();
+            cout << "Classificacao das arestas escrita no arquivo: tree_edges.txt" << endl;
         }
-        outputFileX.close();
-        cout << "Classificacao das arestas que saem do vertice X escrita no arquivo: vertex_edges_classification.txt" << endl;
+        else
+        {
+            cerr << "Error: nao foi possivel escrever no arquivo tree_edges.txt" << endl;
+        }
+
+        // irei colocar na mesma função a escrita no arquivo do resultado das classificações das arestas que saem do verticeX
+        ofstream outputFileX("vertex_edges_classification.txt");
+        if (outputFileX)
+        {
+            outputFileX << "Classificacao das arestas que saem do vertice " << Vx << endl;
+            for (int i = pointer[Vx]; i < pointer[Vx + 1]; i++)
+            {
+                outputFileX << Vx << " -> " << arc_dest[i];
+                switch (edgeClassification[i])
+                {
+                case TREE_EDGE:
+                    outputFileX << " Aresta de árvore" << endl;
+                    break;
+                case BACK_EDGE:
+                    outputFileX << " Aresta de retorno" << endl;
+                    break;
+                case FORWARD_EDGE:
+                    outputFileX << " Aresta de avanço" << endl;
+                    break;
+                case CROSS_EDGE:
+                    outputFileX << " Aresta de cruzamento" << endl;
+                    break;
+                }
+            }
+            outputFileX.close();
+            cout << "Classificacao das arestas que saem do vertice X escrita no arquivo: vertex_edges_classification.txt" << endl;
+        }
+        else
+        {
+            cerr << "Error: nao foi possivel escrever no arquivo vertex_edges_classification.txt" << endl;
+        }
     }
-    else
+    catch (bad_alloc &e)
     {
-        cerr << "Error: nao foi possivel escrever no arquivo vertex_edges_classification.txt" << endl;
+        cout << e.what() << endl;
     }
 }
 
@@ -279,8 +296,7 @@ int main()
             cout << "Out of bounds, digite um vertices contido no grafo: ";
         }
     }
-    classifyEdges(pointer, arc_dest, Vx);
     // numero de vertices = pointer.size() - 2
-
+    classifyEdges(pointer, arc_dest, Vx);
     return 0;
 }
